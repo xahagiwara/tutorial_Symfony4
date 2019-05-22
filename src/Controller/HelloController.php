40a958ae3cec545909fec8fcfbbbeed32327d6c7
xahagiwara <s.hagiwara@xearts.jp>
2019-05-22 13:48:08 +0900

    create_pass

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,18 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class HelloController extends AbstractController
 {
     /**
      * @Route("/hello/{name}/{id}", name="hello")
      */
-    public function index($name, $id)
+    public function index(Request $request, $name, $id)
     {
+        $pass = $request->get('pass');
+
         $resource = [
-                "name" => $name,
-                "id" => $id,
-                ];
+            "name" => $name,
+            "id" => $id,
+            "pass" => $pass
+        ];
 
         return $this->render('hello/index.html.twig', [
             'resouce' => $resource,
